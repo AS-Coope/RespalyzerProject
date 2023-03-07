391b21f187f41d7902b7c6c8287ac5988ddb8d26
ZrC <94488901+AS-Coope@users.noreply.github.com>
2023-03-07 03:40:55 -0800

    Implemented (Temporary) User Details Saving
    - Button stores data (temporarily) when clicked
    - It is displayed in a toast
    -If the phone screen is too small then it won't show the text at the bottom of the screen (but it is there) (a scrollable view needs to be added)

--- app/src/main/java/com/example/respalyzerproject/MainActivity.kt
+++ app/src/main/java/com/example/respalyzerproject/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.respalyzerproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import android.widget.TextView
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // references to the various widgets on (from the activity_main file)
+        val userName = findViewById<EditText>(R.id.userNameField)
+        val userAge = findViewById<EditText>(R.id.userAge)
+        val userWeight = findViewById<EditText>(R.id.userWeight)
+        val userHeight = findViewById<EditText>(R.id.userHeight)
+        val knownIllnesses = findViewById<EditText>(R.id.userHeight)
+        val userEmContactName = findViewById<EditText>(R.id.userEmergencyContactName)
+        val userEmContactNumber = findViewById<EditText>(R.id.userEmergencyContactNumber)
+        val profileScreenSubmitBtn = findViewById<Button>(R.id.profileScreen1NextBtn)
+        val displayUser = findViewById<TextView>(R.id.displayUser)
+
+        profileScreenSubmitBtn.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Name: ${userName.getText().toString()} :: Age: ${userAge.getText().toString()} :: " +
+                    "Weight: ${userWeight.getText().toString()} :: Height: ${userHeight.getText().toString()} :: Known Illnesses: ${knownIllnesses.getText().toString()} :: " +
+                    "Emergency Contact Name: ${userEmContactName.getText().toString()} :: Emergency Contact Number: ${userEmContactNumber.getText().toString()}", Toast.LENGTH_LONG).show()
+            displayUser.text = "Name: ${userName.getText().toString()} :: Age: ${userAge.getText().toString()} :: "
+        }
     }
 }
