706da9189cdb9f3cf0bd4d7e1a541dc3e2ad171a
ZrC <94488901+AS-Coope@users.noreply.github.com>
2023-03-12 18:32:17 -0800

    Created Welcome Screen Activity
    - Welcome Screen Activity added to the AndroidManifest.xml file
    - Data sent from the Profile Screen is displayed at the Welcome Screen
    - Currently not saved in a Data Class but passed using several putExtra() functions inside the Intent (found in the MainActivity.kt file)

--- app/src/main/java/com/example/respalyzerproject/MainActivity.kt
+++ app/src/main/java/com/example/respalyzerproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.respalyzerproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -17,17 +18,55 @@ class MainActivity : AppCompatActivity() {
         val userAge = findViewById<EditText>(R.id.userAge)
         val userWeight = findViewById<EditText>(R.id.userWeight)
         val userHeight = findViewById<EditText>(R.id.userHeight)
-        val knownIllnesses = findViewById<EditText>(R.id.userHeight)
+        val knownIllnesses = findViewById<EditText>(R.id.userKnownIllnesses)
         val userEmContactName = findViewById<EditText>(R.id.userEmergencyContactName)
         val userEmContactNumber = findViewById<EditText>(R.id.userEmergencyContactNumber)
         val profileScreenSubmitBtn = findViewById<Button>(R.id.profileScreen1NextBtn)
         val displayUser = findViewById<TextView>(R.id.displayUser)
 
         profileScreenSubmitBtn.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Name: ${userName.getText().toString()} :: Age: ${userAge.getText().toString()} :: " +
+
+            /*Toast.makeText(this@MainActivity, "Name: ${userName.getText().toString()} :: Age: ${userAge.getText().toString()} :: " +
                     "Weight: ${userWeight.getText().toString()} :: Height: ${userHeight.getText().toString()} :: Known Illnesses: ${knownIllnesses.getText().toString()} :: " +
                     "Emergency Contact Name: ${userEmContactName.getText().toString()} :: Emergency Contact Number: ${userEmContactNumber.getText().toString()}", Toast.LENGTH_LONG).show()
-            displayUser.text = "Name: ${userName.getText().toString()} :: Age: ${userAge.getText().toString()} :: "
+             */
+
+            // checking to ensure all the required fields are filled before submission
+            displayUser.text = ""
+            if (userName.getText().toString() == "" || userAge.getText().toString() == "" || userWeight.getText().toString() == "" || userHeight.getText().toString() == ""){
+                if (userName.getText().toString() == ""){
+                    displayUser.text = "Name is required to be filled out"
+                }
+                if (userAge.getText().toString() == "" ){
+                    displayUser.text = "Age is required to be filled out"
+                }
+                if (userWeight.getText().toString() == "" ){
+                    displayUser.text = "Weight is required to be filled out"
+                }
+                if (userHeight.getText().toString() == "" ){
+                    displayUser.text = "Height is required to be filled out"
+                }
+            } else {
+
+                /*displayUser.text = "Name: ${userName.getText().toString()} :: Age: ${userAge.getText().toString()} :: " +
+                    "Weight: ${userWeight.getText().toString()} :: Height: ${userHeight.getText().toString()} :: Known Illnesses: ${knownIllnesses.getText().toString()} :: " +
+                    "Emergency Contact Name: ${userEmContactName.getText().toString()} :: Emergency Contact Number: ${userEmContactNumber.getText().toString()}"
+                */
+                // goes to the welcome screen
+                Intent(this, WelcomeScreenActivity::class.java).also{
+                    // putExtra allows the passing of data from one activity to the next using an Intent
+                    it.putExtra("EXTRA_USERNAME", userName.text.toString())
+                    it.putExtra("EXTRA_USERAGE", userAge.text.toString().toInt())
+                    it.putExtra("EXTRA_USERWEIGHT", userWeight.text.toString().toFloat())
+                    it.putExtra("EXTRA_USERHEIGHT", userHeight.text.toString().toFloat())
+                    it.putExtra("EXTRA_KNOWNILLNESSES", knownIllnesses.text.toString())
+                    it.putExtra("EXTRA_EMERGNAME", userEmContactName.text.toString())
+                    it.putExtra("EXTRA_EMERGNUMBER", userEmContactNumber.text.toString())
+
+                    // starts the new activity (next screen, in this case)
+                    startActivity(it)
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/respalyzerproject/WelcomeScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.example.respalyzerproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class WelcomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+
+        val showUser = findViewById<TextView>(R.id.tvUserInfo)
+
+        val pUserName = intent.getStringExtra("EXTRA_USERNAME")
+        val pUserAge = intent.getIntExtra("EXTRA_USERAGE", 0)
+        val pUserWeight = intent.getFloatExtra("EXTRA_USERWEIGHT", 0.0f)
+        val pUserHeight = intent.getFloatExtra("EXTRA_USERHEIGHT",  0.0f)
+        val pUserKnownIllnesses = intent.getStringExtra("EXTRA_KNOWNILLNESSES")
+        val pUserEmerName = intent.getStringExtra("EXTRA_EMERGNAME")
+        val pUserEmerNumber = intent.getStringExtra("EXTRA_EMERGNUMBER")
+
+        showUser.text = "Welcome, $pUserName.\nThe following data was registered about you: " +
+                        "Age: $pUserAge\nWeight: $pUserWeight\nHeight: $pUserHeight\nKnown Illnesses: $pUserKnownIllnesses\n" +
+                        "Emergency Contact Name: $pUserEmerName\nEmergency Contact Number: $pUserEmerNumber"
+    }
+}
