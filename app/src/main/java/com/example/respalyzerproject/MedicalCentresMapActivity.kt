0f8863fb12ddc21b36e95fb3c4d09e78f08099b7
ZrC <94488901+AS-Coope@users.noreply.github.com>
2023-04-10 21:33:10 -0800

    Added Empty Activity Medical Centres Map

---
+++ app/src/main/java/com/example/respalyzerproject/MedicalCentresMapActivity.kt
@@ -0,0 +1,11 @@
+package com.example.respalyzerproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MedicalCentresMapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medical_centres_map)
+    }
+}
