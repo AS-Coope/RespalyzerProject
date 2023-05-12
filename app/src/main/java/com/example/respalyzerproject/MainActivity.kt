5c5d41238ffae8f4f1ba1c4f15b11362a702c848
AS-Coope <deltazirconium@gmail.com>
2023-05-11 22:13:50 -0500

    User Saved in Room Database
    - Updated Java version (in gradle to Version 17)
    - Repository stores User Profile Data
    - Architecture implementation for Database involving UserViewModel
    - Data now sourced from the Database when showing to the UI

--- app/src/main/java/com/example/respalyzerproject/MainActivity.kt
+++ app/src/main/java/com/example/respalyzerproject/MainActivity.kt
@@ -7,8 +7,14 @@ import android.widget.Button
 import android.widget.Toast
 import android.widget.TextView
 import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
+import com.example.respalyzerproject.userprofile.User
+import com.example.respalyzerproject.userprofile.UserViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    // the view model that passes data to this UI Component (MainActivity)
+    private lateinit var mUserViewModel: UserViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         val profileScreenSubmitBtn = findViewById<Button>(R.id.profileScreen1NextBtn)
         val displayUser = findViewById<TextView>(R.id.displayUser)
 
+        mUserViewModel = ViewModelProvider(this)[UserViewModel::class.java]
         profileScreenSubmitBtn.setOnClickListener{
 
             /*Toast.makeText(this@MainActivity, "Name: ${userName.getText().toString()} :: Age: ${userAge.getText().toString()} :: " +
@@ -56,6 +63,18 @@ class MainActivity : AppCompatActivity() {
                     "Weight: ${userWeight.getText().toString()} :: Height: ${userHeight.getText().toString()} :: Known Illnesses: ${knownIllnesses.getText().toString()} :: " +
                     "Emergency Contact Name: ${userEmContactName.getText().toString()} :: Emergency Contact Number: ${userEmContactNumber.getText().toString()}"
                 */
+                val uName = userName.text.toString()
+                val uAge = userAge.text
+                val uGender = userGender.text.toString()
+                val uWeight = userWeight.text.toString()
+                val uHeight = userHeight.text.toString()
+                val uIllness = knownIllnesses.text.toString()
+                val uEcName = userEmContactName.text.toString()
+                val uEcNumber = userEmContactNumber.text.toString()
+
+                val user = User(0, uName, Integer.parseInt(uAge.toString()), uGender, uWeight.toFloat(), uHeight.toFloat(), uIllness, uEcName, uEcNumber)
+                mUserViewModel.addUser(user)
+
                 // goes to the welcome screen
                 Intent(this, WelcomeScreenActivity::class.java).also{
                     // putExtra allows the passing of data from one activity to the next using an Intent
--- app/src/main/java/com/example/respalyzerproject/userprofile/UserViewModel.kt
+++ app/src/main/java/com/example/respalyzerproject/userprofile/UserViewModel.kt
@@ -3,13 +3,16 @@ package com.example.respalyzerproject.userprofile
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class UserViewModel(application:Application): AndroidViewModel(application) {
 
-    private val readAllData: LiveData<List<User>>
+    val readAllData: LiveData<List<User>>
+    val userProfile = MutableLiveData<User>()
+
     private val repository: UserRepository
 
     // called first when the user view model is called
@@ -25,6 +28,12 @@ class UserViewModel(application:Application): AndroidViewModel(application) {
     fun addUser(user: User) {
         viewModelScope.launch(Dispatchers.IO) {
             repository.addUser(user)
+        }
+    }
+    fun readUserProfile(){
+        viewModelScope.launch(Dispatchers.IO) {
+            // retrieves the first user (and should be only user) of the app
+            repository.readUser().collect{userProfile.postValue(it)}
         }
     }
 }
