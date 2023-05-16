6209bb8ea641d458969c3e6fb8f037e129de8f15
Peter Thelwell <pthelwell55@gmail.com>
2023-05-16 10:24:24 -0500

    Disease Details

--- app/src/main/java/com/example/respalyzerproject/DiseaseInfoActivity.kt
+++ app/src/main/java/com/example/respalyzerproject/DiseaseInfoActivity.kt
@@ -6,33 +6,19 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 
-class IllnessActivity : AppCompatActivity() {
+class DiseaseInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_illness)
+        setContentView(R.layout.activity_disease_info)
 
         val profileBtn = findViewById<ImageButton>(R.id.dsUserAccount3)
-        val learnMoreBtn = findViewById<Button>(R.id.iLearnMore)
         val medCentreBtn = findViewById<Button>(R.id.iMedicalCentreBtn)
-        val emerContactsBtn = findViewById<Button>(R.id.iEmergencyContact)
 
         profileBtn.setOnClickListener{
             // switch to the name of the Analyze Audio activity when that activity is created
             Intent(this, UserProfileActivity::class.java).also{
                 // starts the new activity (next screen, in this case)
                 startActivity(it)
-            }
-        }
-
-        learnMoreBtn.setOnClickListener {
-            Intent(this, DiseaseInfoActivity::class.java).also{
-                startActivity(it) // travel back to the dashboard screen
-            }
-        }
-
-        emerContactsBtn.setOnClickListener {
-            Intent(this, EmergencyContactsActivity::class.java).also{
-                startActivity(it) // travel back to the dashboard screen
             }
         }
 
